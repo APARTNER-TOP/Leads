757c4ac95eceae1927c53c8da10f360a166b7ae8
Roman Matviy <roman@matviy.pp.ua>
2023-02-18 22:30:25 +0200

    clear project

--- resources/views/dashboard/users/profile/edit.blade.php
+++ resources/views/dashboard/users/profile/edit.blade.php
@@ -18,12 +18,6 @@
                     @include('dashboard.users.profile.partials.update-password-form')
                 </div>
             </div>
-
-            <!-- <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('dashboard.users.profile.partials.delete-user-form')
-                </div>
-            </div> -->
         </div>
     </div>
 </x-app-layout>
