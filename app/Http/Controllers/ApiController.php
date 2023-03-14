d87f55c50ce7b77b08e5593e09ae9d80a45019b4
Roman Matviy <roman@matviy.pp.ua>
2023-03-14 10:17:43 +0200

    fix curl for api2

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -97,8 +97,7 @@ class ApiController extends Controller
     }
 
     //! api lead 2
-    protected static $api2_sandbox = 'http://64.227.60.37/api/email/lead';
-    protected static $api2_production = 'http://64.227.60.37/api/email/lead';
+    protected static $api2 = 'http://64.227.60.37/api/email/lead';
 
     /**
      * Leads 2 Api send.
@@ -135,39 +134,123 @@ class ApiController extends Controller
         $result = $request->all();
 
         $lead_source = LeadSource::find($result['lead_source']);
+        //! "Lead Source Name": "Lead Artisans\\/CarShip.com",
 
         $result['lead_source'] = 'name: '. $lead_source->name . ' email: '. $lead_source->email;
-        // dd($result);
 
+        $first_name = $result['first_name'];
+        $last_name = $result['last_name'];
+        $email = $result['email'];
+        $phone = $result['phone'];
+        $ship_date = $result['ship_date'];
+        $transport_type = $result['transport_type'];
+        $comment_from_shipper = $result['comment_from_shipper'];
+        $origin_city = $result['origin_city'];
+        $origin_state = $result['origin_state'];
+        $destination_city = $result['destination_city'];
+        $destination_state = $result['destination_state'];
 
-        $response = Http::post(env('API_SANDBOX') ? self::$api2_sandbox : self::$api2_production, json_decode($json));
+        $vehicle_make = $result['vehicle_make'];
+        $vehicle_model = $result['vehicle_model'];
+        $vehicle_model_year = $result['vehicle_model_year'];
+        $origin_postal_code = $result['origin_zip'];
+        $destination_postal_code = $result['destination_zip'];
 
-        $response_json = json_encode($response->body());
+        //! start curl
+        $curl = curl_init();
 
-        //! throw error
-        if ($response->failed()) {
-            $res = json_decode($response->body());
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => self::$api2,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '[
+                {
+                    "First Name": "' . $first_name . '",
+                    "Last Name": "' . $last_name . '",
+                    "Email": "' . $email . '",
+                    "Phone": "' . $phone . '",
+                    "Shipper Comment": "' . $comment_from_shipper . '",
+                    "Ship Date": "' . $ship_date . 'T00:00:00",
+                    "Transport Type": "' . $transport_type . '",
+                    "Origin City": "' . $origin_city . '",
+                    "Origin State": "' . $origin_state . '",
+                    "Origin Zip": "' . $origin_postal_code . '",
+                    "Destination City": "' . $destination_city . '",
+                    "Destination State": "' . $destination_state . '",
+                    "Destination Zip": "' . $destination_postal_code . '",
+                    "Vehicles": "' . $vehicle_model_year . '|' . $vehicle_make . '|' . $vehicle_model . '|Car",
+                    "Lead Source Name": "' . $lead_source->name . '",
+                    "Lead Source Email": "' . $lead_source->email . '"
+                }
+            ]',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Accept: application/json',
+                'Cookie: laravel_session_crm=KjKm00m7kWhPj9YIwCGfJYDTaZqkMqXwfEVOEndo; student=83748614; studentNoNav=1'
+            ),
+        ));
 
-            $status  = 'error';
-            $code    = $res->status;
-            $message = 'error';
-            $message = $res->message;
+        $response = curl_exec($curl);
+        curl_close($curl);
 
-            Log::saveData($status, $code, $result, $response_json, $leads_type);
+        $res = json_decode($response);
+
+        $status  = 'error';
+        $code    = $res->status;
+        $message = 'error';
+        // $message = $res->message;
+
+        if($res->status === 200 && $res->code === 'OK' ) {
+            //* Success send form
+            $status  = 'success';
+            $code    = 200;
+            $message = 'Data sent successfully';
+
+            Log::saveData($status, $code, $result, $response, $leads_type);
+
+            return back()->with('success', $message);
+        } else {
+            Log::saveData($status, $code, $result, $response, $leads_type);
 
             return back()
                 ->withInput()
                 ->with('error', $message);
         }
+        //! end curl
 
-        //* Success send form
-        $status  = 'success';
-        $code    = 200;
-        $message = $response->body();
-        $message = 'Data sent successfully';
+        // $response = Http::post(env('API_SANDBOX') ? self::$api2 : self::$api2, json_decode($json));
 
-        Log::saveData($status, $code, $result, $response_json, $leads_type);
+        // $response_json = json_encode($response->body());
 
-        return back()->with('success', $message);
+        // //! throw error
+        // if ($response->failed()) {
+        //     $res = json_decode($response->body());
+
+        //     $status  = 'error';
+        //     $code    = $res->status;
+        //     $message = 'error';
+        //     $message = $res->message;
+
+        //     Log::saveData($status, $code, $result, $response_json, $leads_type);
+
+        //     return back()
+        //         ->withInput()
+        //         ->with('error', $message);
+        // }
+
+        // //* Success send form
+        // $status  = 'success';
+        // $code    = 200;
+        // $message = $response->body();
+        // $message = 'Data sent successfully';
+
+        // Log::saveData($status, $code, $result, $response_json, $leads_type);
+
+        // return back()->with('success', $message);
     }
 }
