6e43b125954ba1c1f6156956ed0d490b248a08d7
Roman Matviy <roman@matviy.pp.ua>
2023-03-27 17:58:20 +0300

    fix error on form 1

--- resources/views/components/form1.blade.php
+++ resources/views/components/form1.blade.php
@@ -59,97 +59,97 @@
 
         <div class="mt-2 mb-2" id="lead1_date_container">
             <x-input-label for="ship_date" :value="__('Ship date')" />
-            <x-text-input id="ship_date" class="date-picker" name="ship_date" type="text" class="mt-1 block w-full" :value="old('ship_date') ?? isset($job) ? $job->data['ship_date'] : ''" required autofocus autocomplete="ship_date" />
+            <x-text-input id="ship_date" class="date-picker" name="ship_date" type="text" class="mt-1 block w-full" :value="old('ship_date') ?? isset($job->data['ship_date'] ) ? $job->data['ship_date'] : ''" required autofocus autocomplete="ship_date" />
             <x-input-error class="mt-2" :messages="$errors->get('ship_date')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="transport_type" :value="__('Transport type')" />
             <select id="transport_type" name="transport_type" class="form-control rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full">
-                <option value="1" @if(old('transport_type') == 1 || isset($job) && $job->data['transport_type'] == 1) selected @endif>{{ __('Open') }}</option>
-                <option value="2" @if(old('transport_type') == 2 || isset($job) && $job->data['transport_type'] == 2) selected @endif>{{ __('Enclosed') }}</option>
-                <option value="3" @if(old('transport_type') == 3 || isset($job) && $job->data['transport_type'] == 3) selected @endif>{{ __('Driveaway') }}</option>
+                <option value="1" @if(old('transport_type') == 1 || isset($job->data['transport_type']) && $job->data['transport_type'] == 1) selected @endif>{{ __('Open') }}</option>
+                <option value="2" @if(old('transport_type') == 2 || isset($job->data['transport_type']) && $job->data['transport_type'] == 2) selected @endif>{{ __('Enclosed') }}</option>
+                <option value="3" @if(old('transport_type') == 3 || isset($job->data['transport_type']) && $job->data['transport_type'] == 3) selected @endif>{{ __('Driveaway') }}</option>
             </select>
             <x-input-error class="mt-2" :messages="$errors->get('transport_type')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="comment_from_shipper" :value="__('Comment from shipper')" />
-            <x-text-input id="comment_from_shipper" name="comment_from_shipper" type="text" class="mt-1 block w-full" :value="old('comment_from_shipper') ?? isset($job) ? $job->data['comment_from_shipper'] : ''" autofocus autocomplete="comment_from_shipper" />
+            <x-text-input id="comment_from_shipper" name="comment_from_shipper" type="text" class="mt-1 block w-full" :value="old('comment_from_shipper') ?? isset($job->data['comment_from_shipper']) ? $job->data['comment_from_shipper'] : ''" autofocus autocomplete="comment_from_shipper" />
             <x-input-error class="mt-2" :messages="$errors->get('comment_from_shipper')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="origin_city" :value="__('Origin city')" />
-            <x-text-input id="origin_city" name="origin_city" type="text" class="mt-1 block w-full" :value="old('origin_city') ?? isset($job) ? $job->data['origin_city'] : ''" autofocus autocomplete="origin_city" />
+            <x-text-input id="origin_city" name="origin_city" type="text" class="mt-1 block w-full" :value="old('origin_city') ?? isset($job->data['origin_city']) ? $job->data['origin_city'] : ''" autofocus autocomplete="origin_city" />
             <x-input-error class="mt-2" :messages="$errors->get('origin_city')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="origin_country" :value="__('Origin country')" />
-            <x-text-input id="origin_country" name="origin_country" type="text" class="mt-1 block w-full" :value="old('origin_country') ?? isset($job) ? $job->data['origin_country'] : ''" autofocus autocomplete="origin_country" />
+            <x-text-input id="origin_country" name="origin_country" type="text" class="mt-1 block w-full" :value="old('origin_country') ?? isset($job->data['origin_country']) ? $job->data['origin_country'] : ''" autofocus autocomplete="origin_country" />
             <x-input-error class="mt-2" :messages="$errors->get('origin_country')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="origin_postal_code" :value="__('Origin postal code')" />
-            <x-text-input id="origin_postal_code" name="origin_postal_code" type="number" class="mt-1 block w-full" :value="old('origin_postal_code') ?? isset($job) ? $job->data['origin_postal_code'] : ''" required autofocus autocomplete="origin_postal_code" />
+            <x-text-input id="origin_postal_code" name="origin_postal_code" type="number" class="mt-1 block w-full" :value="old('origin_postal_code') ?? isset($job->data['origin_postal_code']) ? $job->data['origin_postal_code'] : ''" required autofocus autocomplete="origin_postal_code" />
             <x-input-error class="mt-2" :messages="$errors->get('origin_postal_code')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="origin_state" :value="__('Origin state')" />
-            <x-text-input id="origin_state" name="origin_state" type="text" class="mt-1 block w-full" :value="old('origin_state') ?? isset($job) ? $job->data['origin_state'] : ''" autofocus autocomplete="origin_state" />
+            <x-text-input id="origin_state" name="origin_state" type="text" class="mt-1 block w-full" :value="old('origin_state') ?? isset($job->data['origin_state']) ? $job->data['origin_state'] : ''" autofocus autocomplete="origin_state" />
             <x-input-error class="mt-2" :messages="$errors->get('origin_state')" />
         </div>
         <div class="mt-2 mb-2">
             <x-input-label for="destination_city" :value="__('Destination city')" />
-            <x-text-input id="destination_city" name="destination_city" type="text" class="mt-1 block w-full" :value="old('destination_city') ?? isset($job) ? $job->data['destination_city'] : ''" autofocus autocomplete="destination_city" />
+            <x-text-input id="destination_city" name="destination_city" type="text" class="mt-1 block w-full" :value="old('destination_city') ?? isset($job->data['destination_city']) ? $job->data['destination_city'] : ''" autofocus autocomplete="destination_city" />
             <x-input-error class="mt-2" :messages="$errors->get('destination_city')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="destination_country" :value="__('Destination country')" />
-            <x-text-input id="destination_country" name="destination_country" type="text" class="mt-1 block w-full" :value="old('destination_country') ?? isset($job) ? $job->data['destination_country'] : ''" autofocus autocomplete="destination_country" />
+            <x-text-input id="destination_country" name="destination_country" type="text" class="mt-1 block w-full" :value="old('destination_country') ?? isset($job->data['destination_country']) ? $job->data['destination_country'] : ''" autofocus autocomplete="destination_country" />
             <x-input-error class="mt-2" :messages="$errors->get('destination_country')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="destination_postal_code" :value="__('Destination postal code')" />
-            <x-text-input id="destination_postal_code" name="destination_postal_code" type="number" class="mt-1 block w-full" :value="old('destination_postal_code') ?? isset($job) ? $job->data['destination_postal_code'] : ''" required autofocus autocomplete="destination_postal_code" />
+            <x-text-input id="destination_postal_code" name="destination_postal_code" type="number" class="mt-1 block w-full" :value="old('destination_postal_code') ?? isset($job->data['destination_postal_code']) ? $job->data['destination_postal_code'] : ''" required autofocus autocomplete="destination_postal_code" />
             <x-input-error class="mt-2" :messages="$errors->get('destination_postal_code')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="destination_state" :value="__('Destination state')" />
-            <x-text-input id="destination_state" name="destination_state" type="text" class="mt-1 block w-full" :value="old('destination_state') ?? isset($job) ? $job->data['destination_state'] : ''" autofocus autocomplete="destination_state" />
+            <x-text-input id="destination_state" name="destination_state" type="text" class="mt-1 block w-full" :value="old('destination_state') ?? isset($job->data['destination_state']) ? $job->data['destination_state'] : ''" autofocus autocomplete="destination_state" />
             <x-input-error class="mt-2" :messages="$errors->get('destination_state')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="vehicle_inop" :value="__('Vehicle inop')" />
             <select id="vehicle_inop" name="vehicle_inop" class="form-control rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full">
-                <option value="0" @if(old('vehicle_inop') == 0 || isset($job) && $job->data['vehicle_inop'] == 0) selected @endif>{{ __('No') }}</option>
-                <option value="1" @if(old('vehicle_inop') == 1 || isset($job) && $job->data['vehicle_inop'] == 1) selected @endif>{{ __('Yes') }}</option>
+                <option value="0" @if(old('vehicle_inop') == 0 || isset($job->data['vehicle_inop']) && $job->data['vehicle_inop'] == 0) selected @endif>{{ __('No') }}</option>
+                <option value="1" @if(old('vehicle_inop') == 1 || isset($job->data['vehicle_inop']) && $job->data['vehicle_inop'] == 1) selected @endif>{{ __('Yes') }}</option>
             </select>
             <x-input-error class="mt-2" :messages="$errors->get('vehicle_inop')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="vehicle_make" :value="__('Vehicle make')" />
-            <x-text-input id="vehicle_make" name="vehicle_make" type="text" class="mt-1 block w-full" :value="old('vehicle_make') ?? isset($job) ? $job->data['vehicle_make'] : ''" required autofocus autocomplete="vehicle_make" />
+            <x-text-input id="vehicle_make" name="vehicle_make" type="text" class="mt-1 block w-full" :value="old('vehicle_make') ?? isset($job->data['vehicle_make']) ? $job->data['vehicle_make'] : ''" required autofocus autocomplete="vehicle_make" />
             <x-input-error class="mt-2" :messages="$errors->get('vehicle_make')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="vehicle_model" :value="__('Vehicle model')" />
-            <x-text-input id="vehicle_model" name="vehicle_model" type="text" class="mt-1 block w-full" :value="old('vehicle_model') ?? isset($job) ? $job->data['vehicle_model'] : ''" required autofocus autocomplete="vehicle_model" />
+            <x-text-input id="vehicle_model" name="vehicle_model" type="text" class="mt-1 block w-full" :value="old('vehicle_model') ?? isset($job->data['vehicle_model']) ? $job->data['vehicle_model'] : ''" required autofocus autocomplete="vehicle_model" />
             <x-input-error class="mt-2" :messages="$errors->get('vehicle_model')" />
         </div>
 
         <div class="mt-2 mb-2">
             <x-input-label for="vehicle_model_year" :value="__('Vehicle model year')" />
-            <x-text-input id="vehicle_model_year" name="vehicle_model_year" type="number" class="mt-1 block w-full" :value="old('vehicle_model_year') ?? isset($job) ? $job->data['vehicle_model_year'] : ''" required autofocus autocomplete="vehicle_model_year" />
+            <x-text-input id="vehicle_model_year" name="vehicle_model_year" type="number" class="mt-1 block w-full" :value="old('vehicle_model_year') ?? isset($job->data['vehicle_model_year']) ? $job->data['vehicle_model_year'] : ''" required autofocus autocomplete="vehicle_model_year" />
             <x-input-error class="mt-2" :messages="$errors->get('vehicle_model_year')" />
         </div>
 
@@ -157,7 +157,7 @@
 
         <div class="flex items-center gap-4">
             <x-primary-button>
-                {{ isset($job) ? __('Save') : __('Send') }}
+                {{ isset($job->id) ? __('Save') : __('Send') }}
             </x-primary-button>
 
             @if (session('status') === 'profile-updated')
